49ae580621b6a5ee019da093c0e27edb6d469bba
Avi Sahney <avisahney@Avis-MacBook-Air.local>
2023-07-26 21:39:14 +0530

    Media commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const queryParams = getQueryStringParams();
 const name = queryParams.get('name');
 
 function downloadVideo() {
-    let path = "https://" + ipadd +"/download-video/"+ name;
+    let path = "http://" + ipadd +"/download-video/"+ name;
     console.log(name + "Video is Downloading");
     window.location.href = path;
 }
@@ -16,7 +16,7 @@ function getQueryStringParams() {
 }
 
 function streamVideo(){
-        const videoPath = "https://" + ipadd+"/getVideoLink/" + name;
+        const videoPath = "http://" + ipadd+"/getVideoLink/" + name;
         console.log("videoPath :", videoPath )
         fetch(videoPath)
             .then(response => {
@@ -28,7 +28,7 @@ function streamVideo(){
                     return response.json();
                 }
             }).then(data => {
-                    const vPath = "https://"+ipadd+ data.url;
+                    const vPath = "http://"+ipadd+ data.url;
                     videoSource.setAttribute('src', vPath);
                     videoPlayer.load();
                     console.log(name + " Video is streaming successfully");
@@ -51,7 +51,7 @@ function handleVideoError() {
 
 
 function updateViews() {
-    const apiUrl = "https://"+ipadd+"/getViewCount/" + name;
+    const apiUrl = "http://"+ipadd+"/getViewCount/" + name;
 
     fetch(apiUrl)
         .then(response => response.json())
